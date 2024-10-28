ee8a4677e4af0d3260be619ec0703db1a0c9e07f
Matthew Garry <mhgarry92@gmail.com>
2024-10-27 21:00:02 -0400

    fix toggle

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap bg-transparent rounded-md text-sm font-bold transition-colors  duration-500 ring-offset-background focus-visible:outline-none px-2 py-2 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disable:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap bg-transparent rounded-md text-sm font-bold transition-colors  duration-500 ring-offset-background focus-visible:outline-none px-2 py-2 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disable:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 w-1/3",
   {
     variants: {
       variant: {
